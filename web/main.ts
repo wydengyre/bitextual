dd4d0499fbe7ecea738e34771b06260633e195ce
wydengyre <108271225+wydengyre@users.noreply.github.com>
2023-02-28 19:05:10 +0000

    basic main selection

--- web/main.ts
+++ web/main.ts
@@ -1,1 +1,34 @@
-console.log("let's go");
+const $unloaded = Symbol("unloaded");
+type Unloaded = typeof $unloaded;
+
+let sourceTextArea: HTMLTextAreaElement | Unloaded = $unloaded;
+let sourceLanguageSelect: HTMLSelectElement | Unloaded = $unloaded;
+let translatedTextArea: HTMLTextAreaElement | Unloaded = $unloaded;
+let translatedLanguageSelect: HTMLSelectElement | Unloaded = $unloaded;
+let submitButton: HTMLButtonElement | Unloaded = $unloaded;
+
+function loadDom() {
+  sourceTextArea = document.querySelector("#source-text")!;
+  sourceLanguageSelect = document.querySelector("#source-language")!;
+  translatedTextArea = document.querySelector("#translated-text")!;
+  translatedLanguageSelect = document.querySelector("#translated-language")!;
+  submitButton = document.querySelector("button")!;
+  submitButton.addEventListener("click", submit);
+}
+
+function submit(e: Event) {
+  e.preventDefault();
+  const selections = [
+    sourceTextArea.value,
+    sourceLanguageSelect.value,
+    translatedTextArea.value,
+    translatedLanguageSelect.value,
+  ];
+  console.log(selections.join("\n"));
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", loadDom);
+} else {
+  loadDom();
+}
