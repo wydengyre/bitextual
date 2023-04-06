548353b8d6fed5a20cd98bc73c194c1a07e6532d
wydengyre <108271225+wydengyre@users.noreply.github.com>
2023-04-06 16:44:08 +0100

    run `deno fmt`

--- lib/align.test.ts
+++ lib/align.test.ts
@@ -7,5 +7,8 @@ Deno.test("paragraphs function separates paragraphs", async () => {
   const separated = paragraphs(text);
   assertStrictEquals(separated.length, 3099);
   assertStrictEquals(separated[0], "Gustave Flaubert MADAME BOVARY");
-  assertStrictEquals(separated[204], "Madame Bovary remarqua que plusieurs dames n'avaient pas mis leurs gants dans leur verre.");
+  assertStrictEquals(
+    separated[204],
+    "Madame Bovary remarqua que plusieurs dames n'avaient pas mis leurs gants dans leur verre.",
+  );
 });
--- test/README.md
+++ test/README.md
@@ -2,7 +2,5 @@
 
 Downloaded from Project Gutenberg: https://www.gutenberg.org
 
-Quality Fr->En:
-Global quality of unfiltered align after realign 0.713718
-Correcting a rare bug affecting first bisentences.
-Quality 0.713718
+Quality Fr->En: Global quality of unfiltered align after realign 0.713718
+Correcting a rare bug affecting first bisentences. Quality 0.713718
