15d539abd893418e3400c530022309e7c0b96b57
wydengyre <108271225+wydengyre@users.noreply.github.com>
2023-04-07 17:57:05 +0100

    apply some edits to Bovary
    These make the aligned text look nicer at the beginning.

--- lib/align.test.ts
+++ lib/align.test.ts
@@ -5,10 +5,10 @@ import { paragraphs } from "./align.ts";
 Deno.test("paragraphs function separates paragraphs", async () => {
   const text = await readFixtureString("bovary.french.edited.txt");
   const separated = paragraphs(text);
-  assertStrictEquals(separated.length, 3099);
+  assertStrictEquals(separated.length, 3097);
   assertStrictEquals(separated[0], "Gustave Flaubert MADAME BOVARY");
   assertStrictEquals(
     separated[204],
-    "Madame Bovary remarqua que plusieurs dames n'avaient pas mis leurs gants dans leur verre.",
+    "On versa du vin de Champagne à la glace. Emma frissonna de toute sa peau en sentant ce froid dans sa bouche. Elle n'avait jamais vu de grenades ni mangé d'ananas. Le sucre en poudre même lui parut plus blanc et plus fin qu'ailleurs.",
   );
 });
