b69dc8a784b8c685bd5a31e4b3f6419c6eff79f1
wydengyre <108271225+wydengyre@users.noreply.github.com>
2023-04-29 15:58:08 +0100

    add cloudflare terraform

--- web/terraform/cloudflare.tf
+++ web/terraform/cloudflare.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  cloud {
+    organization = "bitextual"
+
+    workspaces {
+      name = "bitextual"
+    }
+  }
 }
 
 provider "cloudflare" {
@@ -19,10 +27,20 @@ variable "domain" {
   default = "bitextual.net"
 }
 
-resource "cloudflare_record" "www" {
-  zone_id = var.zone_id
-  name    = "www"
-  value   = "203.0.113.10"
-  type    = "A"
-  proxied = true
+resource "cloudflare_zone_settings_override" "terraform_managed_resource_811be2d09604bf9ac4295b0a32d83b87" {
+  zone_id = "811be2d09604bf9ac4295b0a32d83b87"
+  settings {
+    always_use_https          = "on"
+    automatic_https_rewrites  = "on"
+    brotli                    = "on"
+    http3                     = "on"
+    ip_geolocation            = "on"
+    ipv6                      = "on"
+    min_tls_version           = "1.0"
+    security_level              = "medium"
+    server_side_exclude         = "on"
+    ssl                         = "flexible"
+    tls_1_3                     = "on"
+    tls_client_auth             = "off"
+  }
 }
