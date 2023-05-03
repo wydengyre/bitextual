b36ba6d236f5dcee4ff3d37f9a20d640b2c4e1b0
wydengyre <108271225+wydengyre@users.noreply.github.com>
2023-05-03 12:47:34 +0100

    post-deploy tests for redirects

--- web/post-deploy.test.ts
+++ web/post-deploy.test.ts
@@ -8,3 +8,29 @@ Deno.test("site is up", async () => {
   const actual = text.match(/<title>(.*)<\/title>/)?.[1];
   assertStrictEquals(actual, expected);
 });
+
+Deno.test("http redirects to https", () => {
+  // this gets a trailing slash for whatever reason
+  return testRedirect("http://bitextual.net", SITE_URL + "/");
+});
+
+Deno.test("www redirects to apex", () => {
+  return testRedirect("https://www.bitextual.net", SITE_URL);
+});
+
+Deno.test("http www redirects to apex", () => {
+  return testRedirect("http://www.bitextual.net", SITE_URL);
+});
+
+async function testRedirect(source: string, dest: string) {
+  const response = await fetch(source, { redirect: "manual" });
+  const cancelPromise = response.body!.cancel();
+  const code = response.status;
+  const redirectLocation = response.headers.get("location")!;
+  try {
+    assertStrictEquals(code, 301);
+    assertStrictEquals(redirectLocation, dest);
+  } finally {
+    await cancelPromise;
+  }
+}
