145e8c82b445b6b2b62102a0e687c519bc56fbcf
wydengyre <108271225+wydengyre@users.noreply.github.com>
2023-05-21 19:53:26 +0100

    mixpanel proxy

--- web/functions/telemetry/track.ts
+++ web/functions/telemetry/track.ts
@@ -4,9 +4,25 @@ export const onRequestPost: PagesFunction = ({ request }) => {
   const proxiedURL = MIXPANEL_API_ROOT + parsedURL.search;
   console.log("mixpanel proxying", proxiedURL);
 
+  const headers = request.headers;
+  const clientIP = headers.get("CF-Connecting-IP");
+  if (clientIP) {
+    headers.set("X-Real-IP", clientIP);
+
+    const xForwardedFor = headers.get("X-Forwarded-For");
+    if (xForwardedFor) {
+      headers.set("X-Forwarded-For", xForwardedFor + ", " + clientIP);
+    } else {
+      headers.set("X-Forwarded-For", clientIP);
+    }
+
+    headers.set("X-Forwarded-Host", parsedURL.host);
+  }
+
   // add proxying headers
   return fetch(proxiedURL, {
     method: "POST",
+    headers,
     body: request.body,
   });
 };
