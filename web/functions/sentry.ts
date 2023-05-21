a903569b735167daac3a3631c78586ee6f4af8d8
wydengyre <108271225+wydengyre@users.noreply.github.com>
2023-05-21 16:37:09 +0100

    simplify sentry proxy

--- web/functions/sentry.ts
+++ web/functions/sentry.ts
@@ -1,48 +1,38 @@
-const SLUG = "/sentry/";
+export const onRequestPost: PagesFunction = async ({ request }) => {
+  const { readable, writable } = new TransformStream();
+  request.body.pipeTo(writable);
 
-export const onRequestPost: PagesFunction = async (context) => {
-  const request = context.request;
-  const url = new URL(request.url);
+  // We tee the stream so we can pull the header out of one stream
+  // and pass the other straight as the fetch POST body
+  const [header, body] = readable.tee();
 
-  if (url.pathname === SLUG) {
-    const { readable, writable } = new TransformStream();
-    request.body.pipeTo(writable);
+  const decoder = new TextDecoder();
+  let chunk = "";
 
-    // We tee the stream so we can pull the header out of one stream
-    // and pass the other straight as the fetch POST body
-    const [header, body] = readable.tee();
+  const headerReader = header.getReader();
 
-    const decoder = new TextDecoder();
-    let chunk = "";
+  while (true) {
+    const { done, value } = await headerReader.read();
 
-    const headerReader = header.getReader();
+    if (done) {
+      break;
+    }
 
-    while (true) {
-      const { done, value } = await headerReader.read();
+    chunk += decoder.decode(value);
 
-      if (done) {
-        break;
-      }
+    const index = chunk.indexOf("\n");
 
-      chunk += decoder.decode(value);
-
-      const index = chunk.indexOf("\n");
-
-      if (index >= 0) {
-        // Get the first line
-        const firstLine = chunk.slice(0, index);
-        const event = JSON.parse(firstLine);
-        const dsn = new URL(event.dsn);
-        // Post to the Sentry endpoint!
-        console.log("Posting to Sentry", event);
-        return fetch(`https://${dsn.host}/api${dsn.pathname}/envelope/`, {
-          method: "POST",
-          body,
-        });
-      }
+    if (index >= 0) {
+      // Get the first line
+      const firstLine = chunk.slice(0, index);
+      const event = JSON.parse(firstLine);
+      const dsn = new URL(event.dsn);
+      // Post to the Sentry endpoint!
+      console.log("Posting to Sentry", event);
+      return fetch(`https://${dsn.host}/api${dsn.pathname}/envelope/`, {
+        method: "POST",
+        body,
+      });
     }
   }
-
-  // If the above routes don't match, return 404
-  return new Response(null, { status: 404 });
 };
