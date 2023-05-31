e3bfe44b5f10116ed4cf3946e87a0173ef328191
wydengyre <108271225+wydengyre@users.noreply.github.com>
2023-05-31 16:42:53 +0100

    offload mixpanel to worker

--- web/mixpanel-worker.ts
+++ web/mixpanel-worker.ts
@@ -1,12 +1,13 @@
 import mixpanel from "mixpanel-browser";
 
+// note that this only works because we're using a custom build of mixpanel-browser
+// that replaces XHR with fetch
+// see https://github.com/mixpanel/mixpanel-js/issues/304
+
 const MIXPANEL_TOKEN = "95dfbbd102f147a2dc289937aa7109ab";
 
 mixpanel.init(MIXPANEL_TOKEN);
-mixpanel.set_config({
-  "persistence": "localStorage",
-  "api_host": "/telemetry",
-});
+mixpanel.set_config({ api_host: "/telemetry" });
 // @ts-ignore: outdated types
 mixpanel.track_pageview();
 
