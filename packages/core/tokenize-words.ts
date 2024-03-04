c48a7febc52024e34cfa764d9ddb2fea9791a70a
Matt Leon <108271225+wydengyre@users.noreply.github.com>
2024-03-04 14:00:53 +0800

    refactor: move tokenize-words into hunalign
    That's the only place it's used

--- packages/core/tokenize-words.ts
+++
@@ -1,8 +0,0 @@
-export { tokenizeWords };
-
-// https://stackoverflow.com/a/26900132
-const pattern = /[^[A-Za-zÀ-ÖØ-öø-ÿа-я0-9_]+/;
-
-function tokenizeWords(text: string): string {
-	return text.split(pattern).join(" ").trim();
-}
