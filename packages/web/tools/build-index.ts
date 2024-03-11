26285f4f45eb5d10208e9e3e6f55302edf739963
Matt Leon <108271225+wydengyre@users.noreply.github.com>
2024-03-11 13:43:44 +0800

    inline main script in HTML

--- packages/web/tools/build-index.ts
+++ packages/web/tools/build-index.ts
@@ -1,11 +1,17 @@
 import { readFile, writeFile } from "node:fs/promises";
-import path from "node:path";
+import { dirname, resolve } from "node:path";
+import { build } from "esbuild";
 import { simpleGit } from "simple-git";
 import { name as packageName, version } from "../package.json" with {
 	type: "json",
 };
 
-async function build() {
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = dirname(__filename);
+const packageRoot = dirname(__dirname);
+
+async function buildIndex() {
+	const buildScriptP = buildScript();
 	const git = simpleGit();
 	const commitHash = await git.revparse(["HEAD"]);
 	const status = await git.status();
@@ -16,13 +22,40 @@ async function build() {
 		hasUncommittedChanges ? "dirty" : "clean"
 	}`;
 
-	const __filename = new URL(import.meta.url).pathname;
-	const __dirname = path.dirname(__filename);
-	const indexPath = path.resolve(__dirname, "../src/index.html");
-	const indexOut = path.resolve(__dirname, "../dist/index.html");
-	const indexContent = await readFile(indexPath, "utf-8");
-	const outContent = indexContent.replace(VERSION_PLACEHOLDER, VERSION_TAG);
+	const indexPath = resolve(packageRoot, "src/index.html");
+	const indexOut = resolve(packageRoot, "dist/index.html");
+	let outContent = await readFile(indexPath, "utf-8");
+	outContent = outContent.replace(VERSION_PLACEHOLDER, VERSION_TAG);
+
+	const builtScript = await buildScriptP;
+	const scriptTag = `<script>${builtScript}</script>`;
+	outContent = outContent.replace("</body>", `${scriptTag}</body>`);
+
 	return writeFile(indexOut, outContent);
 }
 
-await build();
+async function buildScript() {
+	const entryPoint = resolve(packageRoot, "src/main.ts");
+	const outdir = resolve(packageRoot, "dist");
+	const res = await build({
+		entryPoints: [entryPoint],
+		bundle: true,
+		minify: true,
+		sourcemap: true,
+		format: "esm",
+		write: false,
+		outdir,
+	});
+
+	let mainContent = "";
+	for (const outputFile of res.outputFiles) {
+		if (outputFile.path.endsWith(".map")) {
+			await writeFile(outputFile.path, outputFile.text);
+			continue;
+		}
+		mainContent = outputFile.text;
+	}
+	return mainContent;
+}
+
+await buildIndex();
