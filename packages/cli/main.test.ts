67daa9b69b704643e35f28094191ae4a1e8ce106
Matt Leon <108271225+wydengyre@users.noreply.github.com>
2024-03-15 15:42:42 +0800

    cli tests: use fixturePath (#473)

--- packages/cli/main.test.ts
+++ packages/cli/main.test.ts
@@ -3,6 +3,7 @@ import { readFile } from "node:fs/promises";
 import { dirname, join } from "node:path";
 import { test } from "node:test";
 import { fileURLToPath } from "node:url";
+import { fixturePath } from "@bitextual/test/util.js";
 import { go } from "./main.js";
 
 test("main", async (t) => {
@@ -15,12 +16,8 @@ async function testMain() {
 	const alignedPath = join(__dirname, "test", "bovary.aligned.cli.html");
 	const expected = await readFile(alignedPath, "utf-8");
 
-	const bovaryFrench = fileURLToPath(
-		import.meta.resolve("@bitextual/test/bovary.french.epub"),
-	);
-	const bovaryEnglish = fileURLToPath(
-		import.meta.resolve("@bitextual/test/bovary.english.epub"),
-	);
+	const bovaryFrench = fixturePath("bovary.french.epub");
+	const bovaryEnglish = fixturePath("bovary.english.epub");
 	const result = await go(["--html", bovaryFrench, bovaryEnglish]);
 	const resultText = new TextDecoder().decode(result);
 	assert.strictEqual(resultText, expected);
