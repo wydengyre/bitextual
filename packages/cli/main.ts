22ebc24f96e047887103cc4c0357204c744072a0
Matt Leon <108271225+wydengyre@users.noreply.github.com>
2024-05-01 18:16:29 +0700

    WIP
    might need to wait for TS 5.5

--- packages/cli/main.ts
+++ packages/cli/main.ts
@@ -37,8 +37,8 @@ async function goHtml(sourcePath: string, targetPath: string) {
 		readFile(targetPath),
 	]);
 	const [sourceParas, targetParas] = await Promise.all([
-		toArray(epubParas(sourceEpub)),
-		toArray(epubParas(targetEpub)),
+		Array.fromAsync(epubParas(sourceEpub)),
+		Array.fromAsync(epubParas(targetEpub)),
 	]);
 
 	const sourceText = sourceParas.join("\n");
@@ -80,8 +80,8 @@ async function goEpub(sourceEpubPath: string, targetEpubPath: string) {
 		readFile(targetEpubPath),
 	]);
 	const [sourceParas, targetParas] = await Promise.all([
-		toArray(epubParas(sourceEpub)),
-		toArray(epubParas(targetEpub)),
+		Array.fromAsync(epubParas(sourceEpub)),
+		Array.fromAsync(epubParas(targetEpub)),
 	]);
 
 	const sourceText = sourceParas.join("\n");
@@ -112,14 +112,6 @@ async function goEpub(sourceEpubPath: string, targetEpubPath: string) {
 	const aligned = await alignParas(sourceParas, targetParas, alignConfig);
 	const epub = await generateAlignedEpub(aligned, sourceEpub);
 	return new Uint8Array(epub);
-}
-
-async function toArray<T>(asyncGen: AsyncGenerator<T>): Promise<T[]> {
-	const res: T[] = [];
-	for await (const item of asyncGen) {
-		res.push(item);
-	}
-	return res;
 }
 
 const currentFile = fileURLToPath(import.meta.url);
