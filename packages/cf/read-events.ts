c1b855d4ffb1990405929ee406d29fa685437471
Matt Leon <matt@wydengyre.com>
2024-06-01 09:32:27 -0400

    fix cf analytics read error msg

--- packages/cf/read-events.ts
+++ packages/cf/read-events.ts
@@ -3,7 +3,7 @@ if (!CF_ACCOUNT_ID) {
 	throw new Error("CF_ACCOUNT_ID env var is required");
 }
 if (!CF_ANALYTICS_READ_KEY) {
-	throw new Error("CF_EVENTS_READ_API_KEY env var is required");
+	throw new Error("CF_ANALYTICS_READ_KEY env var is required");
 }
 
 const url = `https://api.cloudflare.com/client/v4/accounts/${CF_ACCOUNT_ID}/analytics_engine/sql`;
