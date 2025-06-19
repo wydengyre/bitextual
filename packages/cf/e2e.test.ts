688f2d5b3e93f694b6321c4e3679b8906898b29d
Matt Leon <matt@wydengyre.com>
2025-06-19 20:11:08 +0200

    WIP

--- packages/cf/e2e.test.ts
+++ packages/cf/e2e.test.ts
@@ -6,21 +6,8 @@ import { test } from "node:test";
 import { fileURLToPath } from "node:url";
 import { fixturePath } from "@bitextual/test/util.js";
 import beautify from "js-beautify";
-import puppeteer, { type Browser, type Page } from "puppeteer";
+import puppeteer from "puppeteer";
 import wranglerConf from "./wrangler.json" with { type: "json" };
-
-async function run() {
-	using _server = startServer();
-	const puppeteerP = startPuppeteer();
-	await waitForServer();
-	using puppeteer = await puppeteerP;
-	const { browser } = puppeteer;
-
-	await Promise.all([
-		runIsolatedTest("404", browser, test404),
-		runIsolatedTest("alignment", browser, testAlignment),
-	]);
-}
 
 const SERVER_PORT = 8788;
 const BOVARY_FRENCH_EPUB_PATH = fixturePath("bovary.french.epub");
@@ -31,48 +18,70 @@ const __dirname = dirname(__filename);
 const DIST_PATH = resolve(__dirname, wranglerConf.pages_build_output_dir);
 const BASE_URL = new URL(`http://localhost:${SERVER_PORT}`).toString();
 const SERVER_LOG_LEVEL = "none";
-await run();
 
-async function testAlignment(page: Page) {
-	const expectedPath = join(__dirname, "test", "aligned.html");
-	const expected = await readFile(expectedPath, "utf8");
+test("e2e", async (t) => {
+	using _server = startServer();
+	const puppeteerP = startPuppeteer();
+	await waitForServer();
+	using puppeteer = await puppeteerP;
+	const { browser } = puppeteer;
 
-	await page.goto(BASE_URL);
+	await t.test("404", async (_) => {
+		// TODO: convert to using context managers
+		const context = await browser.createBrowserContext();
+		const page = await context.newPage();
+		try {
+			await page.goto(`${BASE_URL}/nonexistent`);
 
-	const [sourceFileChooser] = await Promise.all([
-		page.waitForFileChooser(),
-		page.click("#sourceText"),
-	]);
-	await sourceFileChooser.accept([BOVARY_FRENCH_EPUB_PATH]);
+			const pageHTML = await page.evaluate(() => document.body.innerHTML);
+			assert.equal(pageHTML, "404 Not Found\n");
+		} finally {
+			await page.close();
+			await context.close();
+		}
+	});
 
-	const [targetFileChooser] = await Promise.all([
-		page.waitForFileChooser(),
-		page.click("#targetText"),
-	]);
-	await targetFileChooser.accept([BOVARY_ENGLISH_EPUB_PATH]);
+	await t.test("alignment", async (_) => {
+		const expectedPath = join(__dirname, "test", "aligned.html");
+		const expected = await readFile(expectedPath, "utf8");
 
-	await page.waitForFunction(
-		() => !document.querySelector<HTMLButtonElement>("#submit")?.disabled,
-	);
+		const context = await browser.createBrowserContext();
+		const page = await context.newPage();
+		try {
+			await page.goto(BASE_URL);
 
-	await page.click("#submit");
+			const [sourceFileChooser] = await Promise.all([
+				page.waitForFileChooser(),
+				page.click("#sourceText"),
+			]);
+			await sourceFileChooser.accept([BOVARY_FRENCH_EPUB_PATH]);
 
-	await page.waitForFunction(
-		() => document.title === "bitextual: Madame Bovary",
-	);
-	const content = await page.content();
+			const [targetFileChooser] = await Promise.all([
+				page.waitForFileChooser(),
+				page.click("#targetText"),
+			]);
+			await targetFileChooser.accept([BOVARY_ENGLISH_EPUB_PATH]);
 
-	const pageCanonical = canonicalizeHtml(content);
-	const expectedCanonical = canonicalizeHtml(expected);
-	assert.strictEqual(pageCanonical, expectedCanonical);
-}
+			await page.waitForFunction(
+				() => !document.querySelector<HTMLButtonElement>("#submit")?.disabled,
+			);
 
-async function test404(page: Page) {
-	await page.goto(`${BASE_URL}/nonexistent`);
+			await page.click("#submit");
 
-	const pageHTML = await page.evaluate(() => document.body.innerHTML);
-	assert.equal(pageHTML, "404 Not Found\n");
-}
+			await page.waitForFunction(
+				() => document.title === "bitextual: Madame Bovary",
+			);
+			const content = await page.content();
+
+			const pageCanonical = canonicalizeHtml(content);
+			const expectedCanonical = canonicalizeHtml(expected);
+			assert.strictEqual(pageCanonical, expectedCanonical);
+		} finally {
+			await page.close();
+			await context.close();
+		}
+	});
+});
 
 async function startPuppeteer() {
 	const browser = await puppeteer.launch({ headless: true });
@@ -111,21 +120,6 @@ function startServer() {
 	process.on("SIGINT", kill);
 	process.on("SIGTERM", kill);
 	return { [Symbol.dispose]: kill };
-}
-
-async function runIsolatedTest(
-	name: string,
-	browser: Browser,
-	testFn: (page: Page) => Promise<void>,
-) {
-	const context = await browser.createBrowserContext();
-	const page = await context.newPage();
-	try {
-		await test(name, () => testFn(page));
-	} finally {
-		await page.close();
-		await context.close();
-	}
 }
 
 /**
